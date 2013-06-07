2908656dccfb0bd36699d621015a9b1eb9b95e8d
Dan Michael Heggø <danmichaelo@gmail.com>
2013-06-07 11:51:32 +0200

    loggers plugin validates ltid and dokid to make sure the screen was fully updated

--- plugins/loggers.js
+++ plugins/loggers.js
@@ -29,6 +29,22 @@ $.bibduck.plugins.push({
             bibsys.send(this.siste_dokid);
         }
     },
+	
+	valid_ltid: function(ltid) {
+		ltid.replace(' ', '');
+		if (ltid.length !== 10) return false;
+		if (!isNaN(parseInt(ltid.substring(0,1)))) return false;
+		if (!isNaN(parseInt(ltid.substring(1,2)))) return false;
+		if (isNaN(parseInt(ltid.substring(8,10)))) return false;
+		return true;
+	},
+	
+	valid_dokid: function(dokid) {
+		dokid.replace(' ', '');
+		if (dokid.length !== 9) return false;
+		if (isNaN(parseInt(dokid.substring(0,2), 10))) return false;
+		return true;
+	},
 
     update: function (bibsys) {
         var ltid,
@@ -41,7 +57,7 @@ $.bibduck.plugins.push({
             // Finnes det noe som ligner på et LTID på linje 4,
             // (som vi ikke allerede har sett)?
             ltid = bibsys.get(4, 15, 24).trim();
-            if (ltid.length === 10 && ltid !== this.aktiv_ltid) {
+            if (this.valid_ltid(ltid) && ltid !== this.aktiv_ltid) {
                 this.aktiv_ltid = ltid;
                 this.siste_ltid = ltid;
                 logger('LTST for: ' + ltid, 'info');
@@ -53,7 +69,7 @@ $.bibduck.plugins.push({
             // Finnes det noe som ligner på et LTID på linje 4,
             // (som vi ikke allerede har sett)?
             ltid = bibsys.get(18, 18, 27).trim();
-            if (ltid.length === 10 && ltid !== this.aktiv_ltid) {
+            if (this.valid_ltid(ltid) && ltid !== this.aktiv_ltid) {
                 this.aktiv_ltid = ltid;
                 this.siste_ltid = ltid;
                 logger('LTSØK for: ' + ltid, 'info');
@@ -65,8 +81,8 @@ $.bibduck.plugins.push({
         // Er vi på en retur-skjerm?
         if (bibsys.get(2, 1, 15) === 'Returnere utlån') {
             dokid = bibsys.get(6, 31, 39).trim();
-            if (dokid.length === 9 && dokid !== this.siste_retur) {
-                ltid = bibsys.get(15, 16, 25);
+			ltid = bibsys.get(15, 16, 25);
+            if (this.valid_ltid(ltid) && this.valid_dokid(dokid) && dokid !== this.siste_retur) {                
                 this.siste_retur = dokid;
                 this.siste_dokid = dokid;
                 this.siste_ltid = ltid;
@@ -80,14 +96,16 @@ $.bibduck.plugins.push({
         if (this.utlaansskjerm === false && bibsys.get(1, 1, 14) === 'Lån registrert') {
             ltid = bibsys.get(1, 20, 29);
             dokid = bibsys.get(10, 7, 15);
-            logger('Utlån registrert: ' + dokid + ' til ' + ltid, 'info');
-            this.utlaansskjerm = true;
-            this.siste_ltid = ltid;
-            this.siste_dokid = dokid;
-            /*if (confirm("Stikkseddel?") === true) {
-                bibsys.bringToFront();
-                bibsys.typetext('stikk!');
-            }*/
+            if (this.valid_ltid(ltid) && this.valid_dokid(dokid)) {
+				logger('Utlån registrert: ' + dokid + ' til ' + ltid, 'info');
+				this.utlaansskjerm = true;
+				this.siste_ltid = ltid;
+				this.siste_dokid = dokid;
+				/*if (confirm("Stikkseddel?") === true) {
+					bibsys.bringToFront();
+					bibsys.typetext('stikk!');
+				}*/
+			}
         } else if (this.utlaansskjerm === true && bibsys.get(1, 1, 14) !== 'Lån registrert') {
             this.utlaansskjerm = false;
         }
@@ -95,7 +113,7 @@ $.bibduck.plugins.push({
 		// Er vi på en dokstatskjerm?
         if (bibsys.get(2, 1, 38) === 'Utlånsstatus for et dokument (DOkstat)') {		
             dokid = bibsys.get(5, 9, 17);
-			if (dokid.length === 9 && parseInt(dokid.substring(0,2), 10) > 0 && dokid !== this.siste_dokid) {
+			if (this.valid_dokid(dokid) && dokid !== this.siste_dokid) {
 				this.siste_dokid = dokid;
                 logger('Dokstat for: ' + dokid, 'info');
 			}
